ee527aa1633dbe3009f0c6918fe4dc29c1e60f22
Vaibhav Mahendrasing Rajput <157829577+vaibhavrajput07@users.noreply.github.com>
2024-11-14 14:09:19 +0530

    Update ProjectsContainer.jsx

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -4,14 +4,14 @@ export default function ProjectContainer() {
         <div style={{ backgroundColor: "white", marginTop: "60px", marginBottom:"80px", display: "flex", justifyContent: "space-between", flexWrap: "wrap" }}>
             <ProjectCard
                 Title="WanderLust(Full Stack)"
-                url="./src/components/videos/wanderlust.png"
+                url="./videos/wanderlust.png"
                 description="This is the Wanderlust web project developed using front-end technologies, back-end technologies, and a database."
                 learenMore="https://github.com/vaibhavrajput07/ApnaCollege_Project"
                 href="https://apnacollege-project-0qiq.onrender.com/listing"
             />
             <ProjectCard
                 Title="Investment_portfolio_Tracker"
-                url="./src/components/videos/InvestmentTracker.png"
+                url="./videos/InvestmentTracker.png"
                 description="This is an Investment Portfolio Tracker project developed using frontend technologies such as HTML, CSS & JavaScript."
                 learenMore="https://github.com/vaibhavrajput07/InvetsmentPortfolio_Tracker"
                 href="https://invetsment-portfolio-tracker.vercel.app"
@@ -19,7 +19,7 @@ export default function ProjectContainer() {
 
             <ProjectCard
                 Title="Spotify Clone"
-                url="./src/components/videos/Spotify.png"
+                url="./videos/Spotify.png"
                 description="This is a Spotify clone made by me during my course learning.
                                 In this project, I used HTML and CSS technologies."
                 learenMore="https://github.com/vaibhavrajput07/Spotify"
@@ -27,7 +27,7 @@ export default function ProjectContainer() {
             />
             <ProjectCard
                 Title="CatFacts"
-                url="./src/components/videos/CatFact.png"
+                url="./videos/CatFact.png"
                 description="I created a CatFact project using HTML, CSS, and JavaScript (by calling an API). This project is responsive on all devices."
                 learenMore="https://github.com/vaibhavrajput07/CatFact"
                 href="https://cat-fact-chi.vercel.app"
